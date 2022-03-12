9ace0cbb64e4964aa09173eeb51ea98c2a684ebf
rykcod <32191714+rykcod@users.noreply.github.com>
2022-03-12 17:58:33 +0100

    Update massa_bootstrap_finder.py

--- massa_bootstrap_finder.py
+++ massa_bootstrap_finder.py
@@ -11,10 +11,10 @@ ERROR="[ \033[0;31m\033[1m ERROR \033[0m]"
 WARN="[ \033[0;33m\033[1m WARN \033[0m ]"
 INFO="[ \033[0;32m\033[1m INFO \033[0m ]"
 
-BASE_CONFIG_FILE_PATH = "massa/massa-node/base_config/config.toml"
-CONFIG_FILE_PATH = "massa/massa-node/config/config.toml"
-BOOTSTRAPPERS_FILE_PATH = "massa/massa-node/config/bootstrappers.toml"
-CLIENT = "/usr/local/bin/massa_client"
+BASE_CONFIG_FILE_PATH = "/massa/massa-node/base_config/config.toml"
+CONFIG_FILE_PATH = "/massa/massa-node/config/config.toml"
+BOOTSTRAPPERS_FILE_PATH = "/massa/massa-node/config/bootstrappers.toml"
+CLIENT = "/massa/massa-client/massa-client"
 
 TEMPLATE = """
 [official]
