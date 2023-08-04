6f6294c0bf0f72e94e09377f63bbb08699d17e21
Pierre Seznec <petterseznec@gmail.com>
2023-08-04 09:12:30 +0200

    publish testnet 25.2

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.24.0 .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.25.2 .
