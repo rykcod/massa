d7cfb8dd80e5a41ba47bda500c42e01005b6aa8b
Pierre Seznec <petterseznec@gmail.com>
2023-09-08 17:25:20 +0200

    publish testnet 26.1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Massa node + Massa-guard #
-**Last build for Massa testnet Episode 26 release 26.0.0**
+**Last build for Massa testnet Episode 26 release 26.1.0**
 
 ![alt text](https://d33wubrfki0l68.cloudfront.net/7df7d7a57a8dda3cc07aab16121b3e3990cf0893/16ccd/portfolio/massa.png)
 
--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.26.0 .
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.26.1 .
