3753de111115c6d3e000fc2ef03632157a392be1
Pierre Seznec <petterseznec@gmail.com>
2023-12-26 14:42:38 +0100

    use v27.6 (securenet)

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.26.1 .
+
+# This builds the image  for all architectures an publish it on dockerhub
+#DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64/v8 -t peterjah/massa-core --push --build-arg VERSION=TEST.27.6 .
+# This builds the image for amd64 an load it in local docker
+DOCKER_BUILDKIT=1 docker buildx build --platform linux/amd64 -t peterjah/massa-core --load --build-arg VERSION=TEST.27.6  .
