0abfdc4a5e598a23fe659dad538dbb5dad101e4f
Pierre Seznec <petterseznec@gmail.com>
2023-12-28 10:43:42 +0100

    refactor nb wallets check

--- sources/init_copy_host_files.sh
+++ sources/init_copy_host_files.sh
@@ -41,6 +41,14 @@ else
 	cp $PATH_NODE/base_config/config.toml node_config_$VERSION.toml
 fi
 
+# check only one wallet in massa_mount
+nbWallet=$(ls -l | grep 'wallet_.*.yaml' | wc -l)
+if (($nbWallet > 1))
+then
+	warn "ERROR" "Several wallets found in massa_mount. Please keep only one wallet in massa_mount"
+	exit 1
+fi
+
 # Wallet to use
 if [ -e wallet_*.yaml ]
 then
@@ -48,10 +56,6 @@ then
 	green "INFO" "Loading wallet $walletFile from massa_mount to client"
 	mkdir -p $PATH_CLIENT/wallets
 	cp $walletFile $PATH_CLIENT/wallets/
-elif [ "$?" = 2 ]
-then
-	warn "ERROR" "Several wallets found in massa_mount. Please keep only one wallet in massa_mount"
-	exit 1
 elif [ -n $WALLET_PRIVATE_KEY ]
 then
 	green "INFO" "Loading wallet from environement private key"
